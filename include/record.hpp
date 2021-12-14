1e5b8f2e8251979d270b94ed5a66e2a42c6be7fc
Abhishek Chakravarti <abhishek@taranjali.org>
2021-12-14 22:12:53 +0530

    Add missing method delcarations
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- include/record.hpp
+++ include/record.hpp
@@ -10,16 +10,21 @@ namespace fedb {
     class Record {
         public:
             Record();
-            Record(std::vector<Field>&);
-            Record(Record&);
+            Record(const std::vector<Field>&);
+            Record(const Record&);
             ~Record();
 
             size_t len() const;
             Field field(size_t) const;
             void field(size_t, Field&);
 
+            void push_back(const Field&);
+
             std::string operator [](size_t) const;
             std::string operator [](const std::string&) const;
+
+            std::string str() const;
+            std::string json() const;
 
         private:
             std::vector<Field> m_fields;
